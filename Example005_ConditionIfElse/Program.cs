1de428d12ab5fa45955e84f61acb27cebb996a97
Pavel Ostapchuk <paviv84@gmail.com>
2022-11-11 19:08:56 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "катя") // ToLower - переводит все в нижний регистр
+{
+    Console.WriteLine ("Привет любимая!");
+}
+else
+{
+    Console.WriteLine ("Привет, ");
+    Console.WriteLine (username);
+}
