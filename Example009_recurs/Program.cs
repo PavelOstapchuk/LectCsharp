2267ffe9d862600c74e03dddaf5f195599193f03
Pavel Ostapchuk <paviv84@gmail.com>
2022-12-07 14:54:17 +0300

    Example009_recurs add

---
+++ Example009_recurs/Program.cs
@@ -0,0 +1,28 @@
+﻿// задача
+// собрать строку с числами от a до b. a<=b.
+Console.WriteLine(NumbersFor(1,10));
+Console.WriteLine(NumbersRec(1,10));
+Console.WriteLine(NumbersRecV2(1,10));
+
+// итеративный метод
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result+=$"{i} ";
+    }
+    return result;
+}
+
+// метод рекурсии
+string NumbersRec (int a, int b)
+{
+    if (a<=b) return $"{a} " + NumbersRec(a+1,b);
+    else return string.Empty;
+}
+string NumbersRecV2 (int a, int b)
+{
+    if (a<b) return $"{a} " + NumbersRec(a+1,b);
+    else return $"{a}";
+}
